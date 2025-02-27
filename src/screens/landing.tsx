85ca7246e5b7599643be6b03512d43351b5bdd14
George Agai <georgeagai2000@gmail.com>
2025-02-27 14:09:25 +0300

    More UI update

--- src/screens/landing.tsx
+++ src/screens/landing.tsx
@@ -1,21 +1,24 @@
 import Dotlottieanimation from "../components/dotlottieanimation.tsx"
 const animals = "/animations/animals.lottie"
 import logo from "../assets/images/logo.svg"
+import avatar from "../assets/avatars/avatar6.png"
 
 function Landing() {
     return (
         <div className="flex-column-align-center-justify-between main-container">
 
             <div className="flex-justify-between width100 navbar">
-                <img src={logo} alt="image" style={{width: "55px"}}/>
-                <p>Logo</p>
+                <img src={logo} alt="image" style={{ width: "55px" }} />
+                <img src={avatar} alt="image" style={{ width: "38px", border: "2px solid #867070", borderRadius: '50%' }} />
             </div>
 
-            <Dotlottieanimation animationPath={animals} />
+            <div className="animal-animation-container flex-align-center">
+                <Dotlottieanimation animationPath={animals}/>
+            </div>
 
-            <div  className="flex-column-align-center">
+            <div className="flex-column-align-center">
                 <h1 className="number">2</h1>
-                <p className="days">hours</p>
+                <p className="days">Hours</p>
                 <p className="description">Alcohol free</p>
             </div>
 
